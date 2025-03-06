7e076340b657f624b8785b8889c0d1605c52e4d2
gurubrahmam <vgurubrahmam338@gmail.com>
2025-03-07 01:50:25 +0530

    react player install

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -29,8 +29,45 @@ function Home() {
       description: "Competitive Salary",
       data: "Begin your journey stress-free."
     },
-    // ... other objects
-  ];
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae0f56e32a2b11cb90771_icons8-funding-100.png",
+      description: "≥ $10,000 USD in Company Funding",
+      data: "Kickstart your venture with solid funding."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae0f56499a033884a417e_icons8-equity-100.png",
+      description: "≥ 10% Founder Equity",
+      data: "Own your share in what you create."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae2d9fa700f0bce8284f3_aws.png",
+      description: "≥ $100,000 USD AWS Credits",
+      data: "Harness AWS for robust infrastructure."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae0f56f2bdcaefd17dfb7_icons8-chat-gpt-100.png",
+      description: "$1,000 OpenAI Credits",
+      data: "Leverage cutting-edge AI to build smart solutions."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae3f61195ced8cfc850c8_ibm.png",
+      description: "$120,000 USD IBM Cloud Credits",
+      data: "Innovate with IBM Cloud."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae0f508ad153ee0fc9169_icons8-twilio-is-a-cloud-communications-platform-as-a-service-company-100.png",
+      description: "$2,500 Twilio Credits",
+      data: "Powerful APIs for messaging, voice, and video."
+    },
+    {
+      image: "https://cdn.prod.website-files.com/623ae64112adcf772da9687e/676ae0f56499a033884a4146_icons8-airtable-100.png",
+      description: "$2,000 Airtable Credits",
+      data: "Streamline your startup with Airtable."
+    },
+
+  ]
+
+
 
   return (
     <div className='bg-black text-white flex flex-col justify-start items-center'>
